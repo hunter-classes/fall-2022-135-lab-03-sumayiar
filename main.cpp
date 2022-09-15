75120177a45ce76bc8b15cf26c87f97ce14a3271
Sumayia Rashid <sumayiarashid@Sumayia-2.local>
2022-09-15 18:07:27 -0400

    a-c

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@ int main(){
     std::cout << "Enter date: " << std::endl;
     std::cin >> date;
 	std:: cout << get_east_storage(date) << std::endl;
+
     double min_east = get_min_east();
     std::cout << "Minimum storage in East basin: " << min_east << " billion gallons." << std::endl;
+
     double max_east = get_max_east();
     std::cout << "Maximum storage in East basin: " << max_east << " billion gallons." << std::endl;
-    // compare_basins(date);
+
+    std::cout << "Enter date: " << std::endl;
+    std::cin >> date;
+    std:: cout << compare_basins(date) << std::endl;
+    
     return 0;
 }
 
--- reservoir.cpp
+++ reservoir.cpp
@@ -7,18 +7,18 @@
 double get_east_storage(std::string date){ 
     std::ifstream fin("Current_Reservoir_Levels.tsv"); 
     if (fin.fail()) {
-        std::cerr << "File cannot be opened for reading." << std::endl; //c outs errors
-        exit(1); // exit if failed to open the file
+        std::cerr << "File cannot be opened for reading." << std::endl; 
+        exit(1); 
     }
-    std::string junk;        // new string variable
-    getline(fin, junk); // read one line from the file; got the first row and left it alone
+    std::string junk;     
+    getline(fin, junk); 
 
     //initialize variables 
     std::string fileDate;
     double eastSt, eastEl, westSt, westEl;
 
     while(fin >> fileDate >> eastSt >> eastEl >> westSt >> westEl) { 
-        fin.ignore(INT_MAX, '\n'); //ignores rest of the field names 
+        fin.ignore(INT_MAX, '\n'); 
         if (date == fileDate ){
             return eastSt;
         }
@@ -53,7 +53,7 @@ double get_min_east(){ //this function should return the minimum storage in the 
 double get_max_east(){ //this function should return the maximum storage in the East basin in the 2018.
     std::ifstream fin("Current_Reservoir_Levels.tsv"); 
     if (fin.fail()) {
-        std::cerr << "File cannot be opened for reading." << std::endl; //c outs errors
+        std::cerr << "File cannot be opened for reading." << std::endl; 
         exit(1); 
     }
     std::string junk;        
@@ -73,31 +73,36 @@ double get_max_east(){ //this function should return the maximum storage in the 
     return max;
 }
 
-// std::string compare_basins(std::string date){
-//     std::ifstream fin("Current_Reservoir_Levels.tsv"); 
-//     if (fin.fail()) {
-//         std::cerr << "File cannot be opened for reading." << std::endl; //c outs errors
-//         exit(1); 
-//     }
-//     std::string junk;        
-//     getline(fin, junk); 
+std::string compare_basins(std::string date){
+    std::ifstream fin("Current_Reservoir_Levels.tsv"); 
+    if (fin.fail()) {
+        std::cerr << "File cannot be opened for reading." << std::endl; 
+        exit(1); 
+    }
 
-//     std::string fileDate;
-//     double eastSt, eastEl, westSt, westEl;
+    std::string junk;        
+    getline(fin, junk); 
 
-//     while(fin >> fileDate >> eastSt >> eastEl >> westSt >> westEl) { 
-//         fin.ignore(INT_MAX, '\n'); 
-//         if (eastSt > westSt){
-//             std::cout << "East" << std::endl;
-//         }
-//         else if (westSt > eastSt) {
-//             std::cout << "West" << std::endl;
-//         }
-//         else {
-//             std::cout << "Equal" << std::endl;
-//         }
-//     }
-//     fin.close(); 
-//     return 0;
-// }
+    std::string fileDate;
+    double eastSt, eastEl, westSt, westEl;
+
+    std::cout << "Enter a date: " << std::endl;
+
+    while(fin >> fileDate >> eastSt >> eastEl >> westSt >> westEl) { 
+        fin.ignore(INT_MAX, '\n'); 
+        if (date == fileDate){
+            if (eastSt > westSt){
+            return "East";
+            }
+        else if (westSt > eastSt) {
+            return "West";
+            }
+        else {
+            return "Equal";
+            }  
+        }
+    }
+    fin.close(); 
+    //return 0;
+}
 
